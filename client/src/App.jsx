32873270d92c964c71aa3e4dcf2fbbf85587f7c0
vanguyen333 <vanguyen333@yahoo.com>
2023-08-18 13:17:44 -0400

    6:53 basic router works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { Write } from "./pages/Write";
+// import { Home } from "./pages/Home";
+// import { Single } from "./pages/Single";
 
-const router = createBrowserRouter({
-  path: "/",
-  element: <div>Home</div>,
-});
+const router = createBrowserRouter([
+  { path: "/", element: <div>This is Home~</div> },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/write", element: <Write /> },
+  // { path: "/home", element: <Home /> },
+  // { path: "/single", element: <Single /> },
+]);
 function App() {
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
     </>
   );
 }
