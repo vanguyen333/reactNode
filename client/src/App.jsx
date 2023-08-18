496dd6f755e830b85ca27946d2048c8d4414d850
vanguyen333 <vanguyen333@yahoo.com>
2023-08-18 13:38:37 -0400

    8:02 navbar, home, footer displayed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,27 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 import { Write } from "./pages/Write";
-// import { Home } from "./pages/Home";
-// import { Single } from "./pages/Single";
+import { Home } from "./pages/Home";
+import { Single } from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const router = createBrowserRouter([
-  { path: "/", element: <div>This is Home~</div> },
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <Home />
+        <Footer />
+      </div>
+    ),
+  },
   { path: "/register", element: <Register /> },
   { path: "/login", element: <Login /> },
   { path: "/write", element: <Write /> },
-  // { path: "/home", element: <Home /> },
-  // { path: "/single", element: <Single /> },
+  { path: "/home", element: <Home /> },
+  { path: "/single", element: <Single /> },
 ]);
 function App() {
   return (
