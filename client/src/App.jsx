f87a318bf0d55b212ad6a5553209eb480a459926
vanguyen333 <vanguyen333@yahoo.com>
2023-08-18 14:17:30 -0400

    12:36 form created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,44 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import { Register } from "./pages/Register";
-import { Login } from "./pages/Login";
+import Login from "./pages/Login";
 import { Write } from "./pages/Write";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <div>
-        <Navbar />
-        <Home />
-        <Footer />
-      </div>
-    ),
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   { path: "/register", element: <Register /> },
   { path: "/login", element: <Login /> },
@@ -26,9 +48,10 @@ const router = createBrowserRouter([
 ]);
 function App() {
   return (
-    <>
+    <div className="app">
+      <div className="container"></div>
       <RouterProvider router={router} />
-    </>
+    </div>
   );
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,14 @@
-import React from "react";
-
-export const Login = () => {
-  return <div>Login</div>;
+const Login = () => {
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form action="">
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
+
+export default Login;
