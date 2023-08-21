f91039f17e8360697594eb2b73262bafa5a2515f
vanguyen333 <vanguyen333@yahoo.com>
2023-08-21 15:58:05 -0400

    21:12

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
+import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">logo</div>
-        <div className="links">links</div>
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+        </div>
       </div>
     </div>
   );
