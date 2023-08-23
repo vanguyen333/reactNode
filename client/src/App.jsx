0b1f107981523a67c5a97f242504c04c4fc7c65f
vanguyen333 <vanguyen333@yahoo.com>
2023-08-22 22:06:44 -0400

    32:39, same, bg div under pic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import {
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import { Write } from "./pages/Write";
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
 import { Single } from "./pages/Single";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
