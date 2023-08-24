67e313d5abd2f4ecd41cdf53d163bda3cb34c868
vanguyen333 <vanguyen333@yahoo.com>
2023-08-23 22:04:00 -0400

    ignore

---
+++ api/routes/posts.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+  res.json("this is post");
+});
+
+export default router;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios"; // npm i axios
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   const res = await axios.post("")
+  // };
   return (
     <div className="auth">
       <h1 className="">Register</h1>
       <form action="">
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Register</button>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        {/* <button onClick={handleSubmit}>Register</button> */}
         <p>This is an error!</p>
         <span>
           Do you have an account? <Link to="/login">Login</Link>
