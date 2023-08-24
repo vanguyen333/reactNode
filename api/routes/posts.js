981b270e8f563fe38e5d8286ab99ca11074630ce
vanguyen333 <vanguyen333@yahoo.com>
2023-08-24 19:57:52 -0400

    1:04:40, from controllers work

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { addPost } from "../controllers/post.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.json("this is post");
-});
+router.get("/test", addPost);
 
 export default router;
